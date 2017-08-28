88595b0a2ea3b08b612b278d9ca8e2ee9661f654
William Shiao <willshiao@gmail.com>
2017-08-27 23:47:21 -0700

    Set up Vuex and Bootstrap

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">Hello</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 import App from './App';
 import router from './router';
+import store from './store';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
@@ -10,6 +15,7 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App },
 });
