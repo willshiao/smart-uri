c18e84060eb565305af553d244d0847790ade4ee
William Shiao <willshiao@gmail.com>
2017-08-30 18:48:21 -0700

    Make login page look better

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="container">
+<div class="d-flex align-items-center flex-column justify-content-center h-100 bg-dark text-white" id="header">
     <h3>Login</h3>
-    <b-form @submit="onSubmit">
-      <b-form-group label="Email" label-for="email">
-        <b-form-input id="email" type="email" v-model="form.email" required placeholder="Enter email"></b-form-input>
-      </b-form-group>
-
-      <b-form-group label="Password" label-for="password">
-        <b-form-input id="password" type="password" v-model="form.password" required placeholder="Enter password"></b-form-input>
-      </b-form-group>
-
-      <b-button type="submit" variant="primary">Submit</b-button>
+    <b-form @submit="onSubmit" class="col-3 mx-auto">
+        <div class="form-group">
+          <b-form-input id="email" type="email" v-model="form.email" required placeholder="Email" class="form-control-lg"></b-form-input>
+        </div>
+        <div class="form-group">
+            <b-form-input id="password" type="password" v-model="form.password" required placeholder="Password" class="form-control-lg"></b-form-input>
+        </div>
+        <div class="form-group">
+          <b-button type="submit" variant="primary" class="btn-lg btn-block" v-bind:class="{ disabled: loginPending }">Sign In</b-button>
+        </div>
     </b-form>
-  </div>
+</div>
 </template>
 
 <script>
@@ -27,11 +27,13 @@ export default {
         email: '',
         password: '',
       },
+      loginPending: false,
     };
   },
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
+      this.loginPending = true;
       api.login(this.form)
         .then((res) => {
           if(res.data.status === 'success') {
@@ -39,7 +41,12 @@ export default {
             this.$router.push('/');
             return;
           }
-          this.$swal(res.data.message, 'warning');
+          this.loginPending = false;
+          this.$swal({
+            title: 'Failed to log in',
+            text: res.data.message,
+            type: 'warning',
+          });
         });
     },
   },
@@ -47,4 +54,10 @@ export default {
 </script>
 
 <style scoped>
+#header {
+  padding-top: 10%;
+}
+#header > h3 {
+  color: black;
+}
 </style>
