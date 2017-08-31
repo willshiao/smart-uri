aa290799c738819aed5f853ee3ca03cd324de5cf
William Shiao <willshiao@gmail.com>
2017-08-30 23:37:56 -0700

    Add authenticated request method to api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import store from '@/store';
 
 const API_URL = 'http://localhost:3000';
 
@@ -9,6 +10,16 @@ export default {
     return axios.post(`${API_URL}/auth/login`, {
       email,
       password,
+    });
+  },
+
+  request(endpoint, settings = {}) {
+    return axios({
+      url: `${API_URL}/${endpoint}`,
+      headers: {
+        Authorization: `Bearer ${store.state.jwt}`,
+      },
+      ...settings,
     });
   },
 };
