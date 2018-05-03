77643253537fc8ca2dcc7a3a87ba3121a25f4425
William Shiao <willshiao@gmail.com>
2018-05-03 00:51:12 -0700

    Update NPM packages

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 const config = require('config');
-const bcrypt = require('bcrypt-as-promised');
+const bcrypt = require('bcrypt');
 const Promise = require('bluebird');
 
 const Schema = mongoose.Schema;
