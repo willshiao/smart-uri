9c4566f78c2014fcf0084712500c2e012b8948fc
William Shiao <willshiao@gmail.com>
2018-05-03 01:07:47 -0700

    Improve home page

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="container">Hello {{loggedIn}}</div>
+<div class="container">
+  <div v-if="loggedIn">
+    <p>Hello! You are logged in as {{user.email}}.</p>
+    <p>Get started by adding a redirect <router-link to="/redirect">here</router-link>.</p>
+  </div>
+  <div v-else>
+    Welcome to SmartURI.
+  </div>
+</div>
 </template>
 
 <script>
+import store from '@/store';
+
 export default {
   name: 'home',
   data() {
-    return {};
+    return {
+      user: store.getters.user,
+    };
   },
   computed: {
     loggedIn() {
