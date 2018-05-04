d61bf3ebd57dc3e6c2280fabf9a9d839a2ccc77d
William Shiao <willshiao@gmail.com>
2018-05-03 19:55:41 -0700

    Update some packages
    Also implement redirects listing page

--- frontend/src/components/RedirectList.vue
+++ frontend/src/components/RedirectList.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="container">
     <h3>Redirects</h3>
+    <b-table striped hover :items="redirects" :fields='fields'></b-table>
   </div>
 </template>
 
 <script>
+import api from '@/lib/api';
+
 export default {
   name: 'redirect-list',
   data() {
-    return {};
+    return {
+      redirects: [],
+      fields: [
+        'name',
+        'slug',
+        'enabled',
+      ],
+    };
+  },
+  mounted() {
+    api.request('api/redirects', {
+      method: 'GET',
+    }).then((res) => {
+      if(res.data.status === 'success') {
+        this.redirects = res.data.data;
+      }
+    });
   },
 };
 </script>
